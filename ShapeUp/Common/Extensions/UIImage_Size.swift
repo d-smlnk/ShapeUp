547c9c23af941ef7739e23129227a03e9f6193fb
d-smlnk <140155084+d-smlnk@users.noreply.github.com>
2023-12-03 15:19:08 +0100

    added statistics for exercises, fixed bugs with chevron button

---
+++ ShapeUp/Common/Extensions/UIImage_Size.swift
@@ -0,0 +1,8 @@
+//
+//  UIImage_Size.swift
+//  ShapeUp
+//
+//  Created by Дима Самойленко on 27.11.2023.
+//
+
+import Foundation
---
+++ ShapeUp/Views/Cells/StatCVC.swift
@@ -0,0 +1,12 @@
+//
+//  StatCVC.swift
+//  ShapeUp
+//
+//  Created by Дима Самойленко on 02.12.2023.
+//
+
+import UIKit
+
+class StatCVC: UICollectionViewCell {
+    
+}
